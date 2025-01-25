1f5271ca6d7c264c44d6cc129a2e363517a3dcc3
leonahpd <lionaraj97@gmail.com>
2025-01-25 11:14:29 +0530

    Delete frontend/app.js

--- frontend/app.js
+++
@@ -1,6 +0,0 @@
-fetch('http://backend:5000/products')
-    .then(response => response.json())
-    .then(data => {
-        const productDiv = document.getElementById('products');
-        productDiv.innerHTML = data.map(product => `<p>${product.name}: $${product.price}</p>`).join('');
-    });
